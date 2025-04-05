79f3b30ee201b9b20bb85f6a30e7d3acda1a3bf7
iglée42 <igleev2@gmail.com>
2025-04-05 12:51:12 +0200

    Add missing import

--- src/main/java/fr/iglee42/createcasing/utils/ItemChangeBlockManager.java
+++ src/main/java/fr/iglee42/createcasing/utils/ItemChangeBlockManager.java
@@ -12,6 +12,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
